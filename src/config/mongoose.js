9483d10354ad97280d3c880554a0c372c5a179c8
agge <brannmarkdev@gmail.com>
2025-10-01 11:07:26 +0200

    feat: added temp db

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -2,7 +2,9 @@ import m from "mongoose"
 
 const mongoose = async () => {
     try {
-        m.connect("mongodb://127.0.0.1:27017/gomoku")
+        m.connect(
+            "mongodb+srv://admin:test@cluster0.sesey3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        )
         console.log("DB running @ mongodb://127.0.0.1:27017/gomoku")
     } catch (e) {
         console.log(e)
